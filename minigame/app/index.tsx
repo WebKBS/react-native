c04fad6ceaf4f66ba7ec9bdc8f068fb96324e0c6
gangbeomsu <kbs21c1987@gmail.com>
2024-08-03 19:04:18 +0900

    init

---
+++ minigame/app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import {StatusBar} from "expo-status-bar";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
