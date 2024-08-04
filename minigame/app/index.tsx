8835884eb7ae1aa3e4ac36af8c61d04e9dde2a97
gangbeomsu <kbs21c1987@gmail.com>
2024-08-04 13:58:19 +0900

    배경 이미지 추가

--- minigame/app/index.tsx
+++ minigame/app/index.tsx
@@ -1,13 +1,17 @@
 import StartGameScreen from "@/screens/StartGameScreen";
-import {StyleSheet} from "react-native";
+import {ImageBackground, StyleSheet} from "react-native";
 import {LinearGradient} from "expo-linear-gradient";
 
 export default function Page() {
 
   return (
     <>
-      <LinearGradient colors={["#ddb52f", "#4e0329"]} style={styles.rootScreen} start={{x: 0, y: 0}} end={{x: 1, y: 1}}>
-        <StartGameScreen/>
+      <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen} start={{x: 0, y: 0}} end={{x: 1, y: 1}}>
+        <ImageBackground source={require("../assets/images/background.png")} resizeMode="cover"
+                         style={styles.rootScreen}
+                         imageStyle={styles.backgroundImage}>
+          <StartGameScreen/>
+        </ImageBackground>
       </LinearGradient>
 
 
@@ -18,7 +22,9 @@ export default function Page() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
-    backgroundColor: "#ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.15
   }
 });
 
